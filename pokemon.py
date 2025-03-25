1732bc99295f21aac94e452b07dd9555da768e49
jp-cassel <joaopedrocassel@gmail.com>
2025-03-24 22:56:40 -0300

    pokemon and trainer class

---
+++ pokemon.py
@@ -0,0 +1,10 @@
+class teste:
+    def __init__(self, name):
+        self.name = name
+
+def __str__(self):
+    return f"{self.name}"
+
+viado = teste("Viado da bike")
+print(viado)
+print("olho do cu")
